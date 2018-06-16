d159b8d08fcbed6bf015d6b7262987333573b468
CannibalChoco <fudgeblimp@gmail.com>
2018-06-16 07:09:10 +0300

    Add Java library

---
+++ jokes/src/main/java/com/example/jokes/Joke.java
@@ -0,0 +1,8 @@
+package com.example.jokes;
+
+public class Joke {
+
+    public static String getJoke(){
+        return "Want to hear a joke about construction? I'm still working on it.";
+    }
+}
